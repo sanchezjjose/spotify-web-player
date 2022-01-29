a1036bf42ab9750cd5f03591bca41377135a4d80
josanche <jose.sanchez@disneystreaming.com>
2022-01-29 14:34:08 +0100

    Gradients, dark mode and click improvements

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -1,22 +1,44 @@
+import { SyntheticEvent, useState } from 'react';
 import Image from 'next/image';
 import { useAppSelector } from 'redux/hooks';
 import { selectPlayerState } from 'redux/reducers/playerStateSlice';
 import PlayerControls from './PlayerControls';
 import styles from 'styles/NowPlaying.module.scss';
-import audioWave from '../public/icons8-audio-wave.gif';
+import audioWave from 'public/icons8-audio-wave.gif';
+import downArrow from 'public/light-icons8-down-24.png';
 
 export default function NowPlaying({ player }: any) {
+  const [isMaximized, setIsMaximized] = useState<boolean>(false);
+
   const playerState = useAppSelector(selectPlayerState);
   const currentTrack = playerState.track_window?.current_track;
   const artists = currentTrack?.artists.map((artist: Record<string, any>) => artist.name).join(', ');
   const trackName = currentTrack?.name;
   const albumArt = currentTrack?.album.images[0].url;
+  const albumArtSize = isMaximized ? 300 : 70;
+
+  function handleNowPlayingBarClick(e: SyntheticEvent) {
+    const isSpotifyPlayerControl = (e.target as HTMLElement).classList.value.includes('SpotifyPlayer');
+    const shouldMaximize = !isMaximized && !isSpotifyPlayerControl;
+    if (shouldMaximize) {
+      setIsMaximized(!isMaximized);
+    }
+  }
+
+  function handleMinimizeClick() {
+    setIsMaximized(!isMaximized);
+  }
 
   return (
-    <div className={styles.NowPlaying}>
+    <div className={`${styles.NowPlaying} ${isMaximized && styles.maximized}`} onClick={handleNowPlayingBarClick}>
+      {isMaximized &&
+        <div className={styles.downArrow} onClick={handleMinimizeClick}>
+          <Image src={downArrow} width={24} height={24} alt='Down Arrow' />
+        </div>
+      }
       {albumArt &&
           <div className={styles.albumArt}>
-            <Image src={albumArt} width={70} height={70} alt='Album Art' />
+            <Image src={albumArt} width={albumArtSize} height={albumArtSize} alt='Album Art' />
           </div>
       }
       {!playerState.paused &&
